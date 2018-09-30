a91aa2787482a7c582dc59b75a2f5202042e336f
hzwanghaijiang <hzwanghaijiang@corp.netease.com>
2018-09-30 18:16:00 +0800

    lazy load

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import { cube } from './math.js';
-function component() {
-  var element = document.createElement('pre');
+import _ from 'lodash';
 
-  element.innerHTML = [
-    'Hello webpack!',
-    '5 cubed is equal to ' + cube(5)
-  ].join('\n\n');
+function component() {
+  var element = document.createElement('div');
+  var button = document.createElement('button');
+  var br = document.createElement('br');
+
+  button.innerHTML = 'Click me and look at the console!';
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.appendChild(br);
+  element.appendChild(button);
+
+  // Note that because a network request is involved, some indication
+  // of loading would need to be shown in a production-level site/app.
+  button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+    var print = module.default;
+
+    print();
+  });
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,9 @@ module.exports = {
   ],
   output: {
     filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
+    publicPath: './'
   },
   devServer: {
     contentBase: './dist',
